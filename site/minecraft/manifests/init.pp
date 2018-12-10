3eac0c4f409a117e7f5419a0ee0724d40a047bc9
rustycolbert <40500307+rustycolbert@users.noreply.github.com>
2018-12-10 10:09:58 -0600

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,19 @@
+class minecraft {
+  file {'opt/minecrft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft/server.jar':
+    ensure=> file,
+    source => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+    }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    content => 'puppet:///modules/minecraft/minecraft.service',
+ }
